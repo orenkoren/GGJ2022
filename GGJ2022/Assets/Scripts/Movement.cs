09f65053db2af6d3ce2fa546b678014de54b81d7
orenkoren <oren-k@hotmail.com>
2022-01-27 21:40:38 +0200

    More movement

--- GGJ2022/Assets/Scripts/Movement.cs
+++ GGJ2022/Assets/Scripts/Movement.cs
@@ -6,16 +6,32 @@ public class Movement : MonoBehaviour
 {
     public float speed;
     public float jumpStrength;
+
+    private bool isGrounded;
+
     private void Update()
     {
         var input = Input.GetAxis("Horizontal");
         transform.position = new Vector3(transform.position.x + input * speed, transform.position.y, transform.position.z);
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
             Jump();
     }
 
     private void Jump()
     {
         GetComponent<Rigidbody>().AddForce(new Vector3(0, jumpStrength, 0));
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag == "Ground")
+            isGrounded = true;
+
+    }
+
+    private void OnCollisionExit(Collision other)
+    {
+        if (other.gameObject.tag == "Ground")
+            isGrounded = false;
     }
 }
---
+++ GGJ2022/Assets/Scripts/Shoot.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shoot : MonoBehaviour
+{
+    public GameObject projectile;
+
+    private void Update()
+    {
+    }
+}
