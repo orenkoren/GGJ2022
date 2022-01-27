fef6ff6ee1033717f15684a706b6eae5c99ab3ed
Tg121 <50524120+Tg121@users.noreply.github.com>
2022-01-27 22:09:06 +0200

    Add files via upload

--- GGJ2022/Assets/Scripts/Movement.cs
+++ GGJ2022/Assets/Scripts/Movement.cs
@@ -9,17 +9,22 @@ public class Movement : MonoBehaviour
 
     private bool isGrounded;
 
-    private void Update()
+    private void Start()
+    {
+        speed = 1f;
+        jumpStrength = 1f;
+    }
+    private void FixedUpdate()
     {
         var input = Input.GetAxis("Horizontal");
-        transform.position = new Vector3(transform.position.x + input * speed, transform.position.y, transform.position.z);
+        transform.position = new Vector2(transform.position.x + input * speed, transform.position.y);
         if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
             Jump();
     }
 
     private void Jump()
     {
-        GetComponent<Rigidbody>().AddForce(new Vector3(0, jumpStrength, 0));
+        GetComponent<Rigidbody>().AddForce(new Vector2(jumpStrength, 0));
     }
 
     private void OnCollisionEnter(Collision other)
